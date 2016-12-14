56a9bb6d2932b87761ac76eb8df383b747a0d4fb
Matthew Daniels <me@mattdaniels.com.au>
2016-12-14 16:05:08 +1000

    fix(provider): GTM event labels
    * Update to angulartics2-gtm.ts to fix Event Labels
    
    Event label was being passed to GTM on line 63, the code read:
    `targetProperties: properties.label`
    
    When it should have been:
    `label: properties.label`
    
    * Aligned tests to fix to GTM Provider
    
    * Fixed exception tracking to match the event fix

--- src/providers/gtm/angulartics2-gtm.spec.ts
+++ src/providers/gtm/angulartics2-gtm.spec.ts
@@ -43,7 +43,7 @@ describe('Angulartics2GoogleTagManager', () => {
           fixture = createRoot(RootCmp);
           angulartics2.eventTrack.next({ action: 'do', properties: { category: 'cat' } });
           advance(fixture);
-          expect(dataLayer).toContain({ event: 'interaction', target: 'cat', action: 'do', targetProperties: undefined, value: undefined, interactionType: undefined, userId: null });
+          expect(dataLayer).toContain({ event: 'interaction', target: 'cat', action: 'do', label: undefined, value: undefined, interactionType: undefined, userId: null });
       })));
 
   it('should track exceptions',
@@ -52,7 +52,7 @@ describe('Angulartics2GoogleTagManager', () => {
           fixture = createRoot(RootCmp);
           angulartics2.exceptionTrack.next({ appId: 'app', appName: 'Test App', appVersion: '0.1' });
           advance(fixture);
-          expect(dataLayer).toContain({ event: 'interaction', target: 'Exception', action: 'Exception thrown for Test App <app@0.1>', targetProperties: undefined, value: undefined, interactionType: undefined, userId: null });
+          expect(dataLayer).toContain({ event: 'interaction', target: 'Exception', action: 'Exception thrown for Test App <app@0.1>', label: undefined, value: undefined, interactionType: undefined, userId: null });
       })));
 
   it('should set username',
