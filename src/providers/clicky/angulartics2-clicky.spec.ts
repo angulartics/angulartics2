2305587632fb24d377c87a50a4249d2ebbae843d
Nick Celestin <developer@nickcelestin.com>
2017-09-14 17:54:37 -0700

    clicky is up and running

--- src/providers/clicky/angulartics2-clicky.spec.ts
+++ src/providers/clicky/angulartics2-clicky.spec.ts
@@ -4,6 +4,7 @@ import { TestBed, ComponentFixture, fakeAsync, inject } from '@angular/core/test
 import { TestModule, RootCmp, advance, createRoot } from '../../test.mocks';
 import { Angulartics2 } from '../../core/angulartics2';
 import { Angulartics2Clicky } from './angulartics2-clicky';
+import { Title } from '@angular/platform-browser';
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
 declare var window: any;
 
@@ -19,26 +20,60 @@ fdescribe('Angulartics2Clicky', () => {
             ],
             providers: [
                 { provide: Location, useClass: SpyLocation },
-                Angulartics2Clicky
+                Angulartics2Clicky,
+                Title
             ]
         });
-        window.clicky = clicky = jasmine.createSpyObj('clicky', ['log', 'goal']);
-        window.clicky_custom = clicky_custom = {};
+        window.console = jasmine.createSpyObj('console', ['warn', 'log'])
     });
 
-    it('should exist', fakeAsync(inject([Location, Angulartics2, Angulartics2Clicky],
-    (location: Location, angulartics2: Angulartics2, angulartics2Clicky: Angulartics2Clicky) => {
-        fixture = createRoot(RootCmp);
-        expect(angulartics2).not.toBeUndefined();
-        expect(clicky).not.toBeUndefined();
-        expect(clicky_custom).not.toBeUndefined()
-    })))
-    
-    it('should track pages', fakeAsync(inject([Location, Angulartics2, Angulartics2Clicky],
-    (location: Location, angulartics2: Angulartics2, angulartics2Clicky: Angulartics2Clicky) => {
-        fixture = createRoot(RootCmp);
-        angulartics2.pageTrack.next({path: '/abc', location: location});
-        advance(fixture);
-        expect(clicky.log).toHaveBeenCalledWith('/abc');
-    })))
+    describe('on init', () => {
+        beforeEach(() => {
+            window.console = jasmine.createSpyObj('console', ['warn', 'log'])
+        });
+
+        it('should complain if clicky is not found', fakeAsync(inject([Location, Angulartics2, Angulartics2Clicky],
+            (location: Location, angulartics2: Angulartics2, angulartics2Clicky: Angulartics2Clicky) => {
+                window.clicky = undefined;
+                fixture = createRoot(RootCmp);
+                advance(fixture);
+                expect(console.warn).toHaveBeenCalled();
+            })))
+    })
+
+    describe('while active', () => {
+
+        beforeEach(() => {
+            window.clicky = clicky = jasmine.createSpyObj('clicky', ['log', 'goal']);
+        });
+
+        it('should track pages', fakeAsync(inject([Location, Angulartics2, Angulartics2Clicky, Title],
+            (location: Location, angulartics2: Angulartics2, angulartics2Clicky: Angulartics2Clicky, titleService: Title) => {
+                fixture = createRoot(RootCmp);
+                let title: string = 'clicky';
+                titleService.setTitle(title);
+                angulartics2.pageTrack.next({ path: '/abc' });
+                advance(fixture);
+                expect(clicky.log).toHaveBeenCalledWith('/abc', title, 'pageview');
+            })))
+
+        it('should track events', fakeAsync(inject([Location, Angulartics2, Angulartics2Clicky],
+            (location: Location, angulartics2: Angulartics2, angulartics2Clicky: Angulartics2Clicky) => {
+                fixture = createRoot(RootCmp);
+                angulartics2.eventTrack.next({ action: 'do', properties: { title: 'thing', type: 'click' } });
+                advance(fixture);
+                expect(clicky.log).toHaveBeenCalledWith('do', 'thing', 'click');
+            })))
+
+        it('should track goals', fakeAsync(inject([Location, Angulartics2, Angulartics2Clicky],
+            (location: Location, angulartics2: Angulartics2, angulartics2Clicky: Angulartics2Clicky) => {
+                fixture = createRoot(RootCmp);
+                angulartics2.eventTrack.next({ action: 'do', properties: { goal: 1, revenue: 50, noQueue: true } });
+                advance(fixture);
+                expect(clicky.goal).toHaveBeenCalledWith(1, 50, true);
+            })))
+    })
+
+
+
 })
--- src/providers/clicky/angulartics2-clicky.ts
+++ src/providers/clicky/angulartics2-clicky.ts
@@ -1,21 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Angulartics2 } from '../../core/angulartics2';
 
 declare var clicky: any;
+declare var window: any;
 
 @Injectable()
 export class Angulartics2Clicky {
+    
     constructor(
-        private angulartics2: Angulartics2
+        private angulartics2: Angulartics2,
+        private titleService: Title
     ) {
-        if( typeof (clicky) === 'undefined'){
-            console.warn('Clicky not found');
+        if( typeof (clicky) === 'undefined' ) {
+            console.warn('Angulartics 2 Clicky Plugin: clicky global not found');
         }
-        this.angulartics2.pageTrack.subscribe((x: any) => this.pageTrack(x.path, x.location));
+        this.angulartics2.pageTrack.subscribe((x: any) => this.pageTrack(x.path));
+        this.angulartics2.eventTrack.subscribe((x: any) => this.eventTrack(x.action, x.properties));
     }
 
-    pageTrack(path:string, location:any){
-        clicky.log(path);
+    pageTrack (path:string): void {
+        let title: string = this.titleService.getTitle();
+        clicky.log(path, title, 'pageview');
     }
+
+    eventTrack (action:string, properties:any): void {
+        if( typeof (properties.goal) === 'undefined' ) {
+            let title: string = properties.title || null;
+            let type: string = (properties.eventType != null)? this.validateType(properties.eventType) : null;
+            clicky.log(action, title, type);
+        } else {
+            let goalId = properties.goal;
+            let revenue = this.validateNumber(properties.revenue) ? properties.revenue : null;
+            let noQueue = !!properties.noQueue;
+            clicky.goal(goalId, revenue, noQueue);
+        }
+    }
+
+    validateType (type: string): string {
+        const TYPE_ENUM = ['click', 'download', 'outbound', 'pageview'];
+        return (TYPE_ENUM.indexOf(type) >= 0) ? type : 'pageview';
+    }
+
+    validateNumber (number: number): boolean {
+        return typeof number === 'number' && isFinite(number);
+      }
     
 }
