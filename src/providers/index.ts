8c8f3378153ff2108831b95b548a804e071326c1
Barna Tóth <toakak@gmail.com>
2017-10-20 19:12:33 +0200

    feat(providers): Add Intercom tracking provider support (#157)
    feat(provider): Add Intercom tracking provider support

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -5,6 +5,7 @@ export * from './kissmetrics/angulartics2-kissmetrics';
 export * from './mixpanel/angulartics2-mixpanel';
 export * from './piwik/angulartics2-piwik';
 export * from './segment/angulartics2-segment';
+export * from './intercom/angulartics2-intercom';
 export * from './facebook/angulartics2-facebook';
 export * from './appinsights/angulartics2-appinsights';
 export * from './hubspot/angulartics2-hubspot';
