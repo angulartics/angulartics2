0f776eb566be53a5a6796ada5fc46543be361a7d
Scott Cooper <scttcper@gmail.com>
2017-11-21 11:21:29 -0800

    declare exports

--- src/lib/core/public_api.ts
+++ src/lib/core/public_api.ts
@@ -1,4 +1,5 @@
-export * from './angulartics2';
-export * from './angulartics2.module';
-export * from './angulartics2-token';
+export { Angulartics2 } from './angulartics2';
+export { Angulartics2Module } from './angulartics2.module';
+export { ANGULARTICS2_TOKEN, Angulartics2Token } from './angulartics2-token';
+export { Angulartics2On } from './angulartics2On';
 export * from './angulartics2-config';
