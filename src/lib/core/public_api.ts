f7a1f83276c8e4a5e22f5bd5e34210872e560d90
Scott Cooper <scttcper@gmail.com>
2017-12-27 00:22:02 -0800

    export publicly

--- src/lib/core/public_api.ts
+++ src/lib/core/public_api.ts
@@ -4,3 +4,5 @@ export { ANGULARTICS2_TOKEN, Angulartics2Token } from './angulartics2-token';
 export * from './angulartics2-interfaces';
 export { Angulartics2On } from './angulartics2On';
 export * from './angulartics2-config';
+export { RouterlessTracking, TrackNavigationEnd } from './routing/routerless';
+export { AngularRouterTracking } from './routing/angular-router';
