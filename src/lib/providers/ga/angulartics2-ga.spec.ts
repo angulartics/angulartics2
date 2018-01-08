2d9901394a937c89f6471d3afd1aad1464c4ed3c
Vincent <vincent.durnez@kametventures.com>
2018-01-08 15:07:46 +0000

    GA provider - manage all cases where "tracker" is set (i.e. angulartics2.settings.ga.additionalAccountNames is set) * pageTrack(path) => add ga(accountName + '.set', '&uid', userId) * exceptionTrack(properties) => add ga(accountName + '.send', 'exception', eventOptions) * userTimings(properties) => add ga(accountName + '.send', 'timing', properties)

--- src/lib/providers/ga/angulartics2-ga.spec.ts
+++ src/lib/providers/ga/angulartics2-ga.spec.ts
@@ -42,28 +42,36 @@ describe('Angulartics2GoogleAnalytics', () => {
     fakeAsync(inject([Location, Angulartics2, Angulartics2GoogleAnalytics],
         (location: Location, angulartics2: Angulartics2, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) => {
           fixture = createRoot(RootCmp);
+          angulartics2.settings.ga.additionalAccountNames.push('additionalAccountName');
           angulartics2.eventTrack.next({ action: 'do', properties: { category: 'cat' } });
           advance(fixture);
           expect(ga).toHaveBeenCalledWith('send', 'event', { eventCategory: 'cat', eventAction: 'do', eventLabel: undefined, eventValue: undefined, nonInteraction: undefined, page: '/context.html', userId: null, hitCallback: undefined });
+          expect(ga).toHaveBeenCalledWith('additionalAccountName.send', 'event', { eventCategory: 'cat', eventAction: 'do', eventLabel: undefined, eventValue: undefined, nonInteraction: undefined, page: '/context.html', userId: null, hitCallback: undefined });
       })));
 
   it('should track events with hitCallback',
     fakeAsync(inject([Location, Angulartics2, Angulartics2GoogleAnalytics],
         (location: Location, angulartics2: Angulartics2, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) => {
+          angulartics2.settings.ga.additionalAccountNames.push('additionalAccountName');
+
           fixture = createRoot(RootCmp);
           const callback = function() { };
           angulartics2.eventTrack.next({ action: 'do', properties: { category: 'cat', hitCallback: callback } });
           advance(fixture);
           expect(ga).toHaveBeenCalledWith('send', 'event', { eventCategory: 'cat', eventAction: 'do', eventLabel: undefined, eventValue: undefined, nonInteraction: undefined, page: '/context.html', userId: null, hitCallback: callback });
+          expect(ga).toHaveBeenCalledWith('additionalAccountName.send', 'event', { eventCategory: 'cat', eventAction: 'do', eventLabel: undefined, eventValue: undefined, nonInteraction: undefined, page: '/context.html', userId: null, hitCallback: callback });
       })));
 
   it('should track exceptions',
     fakeAsync(inject([Location, Angulartics2, Angulartics2GoogleAnalytics],
         (location: Location, angulartics2: Angulartics2, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) => {
+          angulartics2.settings.ga.additionalAccountNames.push('additionalAccountName');
+
           fixture = createRoot(RootCmp);
           angulartics2.exceptionTrack.next({ fatal: true, description: 'test' });
           advance(fixture);
           expect(ga).toHaveBeenCalledWith('send', 'exception', { exFatal: true, exDescription: 'test' });
+          expect(ga).toHaveBeenCalledWith('additionalAccountName.send', 'exception', { exFatal: true, exDescription: 'test' });
       })));
 
   it('should set username',
@@ -109,9 +117,11 @@ describe('Angulartics2GoogleAnalytics', () => {
     fakeAsync(inject([Location, Angulartics2, Angulartics2GoogleAnalytics],
         (location: Location, angulartics2: Angulartics2, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) => {
           fixture = createRoot(RootCmp);
+          angulartics2.settings.ga.additionalAccountNames.push('additionalAccountName');
           angulartics2.userTimings.next({ timingCategory: 'cat', timingVar: 'var', timingValue: 100 });
           advance(fixture);
           expect(ga).toHaveBeenCalledWith('send', 'timing', { timingCategory: 'cat', timingVar: 'var', timingValue: 100 });
+          expect(ga).toHaveBeenCalledWith('additionalAccountName.send', 'timing', { timingCategory: 'cat', timingVar: 'var', timingValue: 100 });
       })));
 
 });
