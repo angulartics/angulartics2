01eadb1e646962f80d3ca97eb7a53222492861ad
Evgeny <makovetsky.evgeny@gmail.com>
2018-05-17 19:28:27 -0400

    feat: Add 'IBM Digital Analytics' Provider (#269)
    feat: Add 'IBM Digital Analytics' Provider

---
+++ src/lib/providers/ibm-digital-analytics/README.md
@@ -0,0 +1,122 @@
+<img 
+    src="../../../assets/svg/ibm.svg" 
+    alt="IBM logo"
+    height="300px"
+    width="400px" />
+
+# IBM Digital Analytics
+__homepage__: [https://digitalanalytics.ibmcloud.com](https://digitalanalytics.ibmcloud.com)  
+__docs__: [https://www.ibm.com/support/knowledgecenter/SSPG9M/Analytics/kc_welcome_analytics.html](https://www.ibm.com/support/knowledgecenter/SSPG9M/Analytics/kc_welcome_analytics.html)  
+__import__: `import { Angulartics2IBMDigitalAnalytics } from 'angulartics2/ibm-digital-analytics';`  
+
+## Setup
+
+Add the following IBM Digital Analytics code to your *index.html* file, before the closing body tag. Replace *XXXXXXXXX* with your Client ID and other *cmSetClientID* function parameters accordingly.
+
+```html
+<script type="text/javascript" src="//libs.coremetrics.com/eluminate.js"></script>
+<script type="text/javascript">
+    cmSetClientID("XXXXXXXXX", true, "data.coremetrics.com", "ibm.com");
+</script>
+
+```
+
+## Usage
+
+### Import
+```ts
+// app.module.ts
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, RouterOutlet } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+import { Angulartics2Module } from 'angulartics2';
+import { Angulartics2IBMDigitalAnalytics } from 'angulartics2/ibm-digital-analytics';
+import { PageComponent } from './page/page.component';
+import { HomeComponent } from './home/home.component';
+
+const ROUTES: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'page', component: PageComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    Angulartics2Module.forRoot([Angulartics2IBMDigitalAnalytics])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+```
+
+### Enable Auto Route Logging
+```ts
+// app.component.ts 
+import { Component } from '@angular/core';
+import { Angulartics2IBMDigitalAnalytics } from 'angulartics2/ibm-digital-analytics';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // Enable auto route logging
+  constructor(angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) {}
+}
+```
+
+### Track Events in Template
+```html
+<div 
+    angulartics2On="click" 
+    angularticsAction="cmCreateProductviewTag" 
+    [angularticsProperties]="{'productId': '123', 'productName': 'Apples', 'categoryId': '777'}"
+>Click Me for cmCreateProductviewTag</div>
+
+<div 
+    angulartics2On="click" 
+    angularticsAction="cmCreateShopAction5Tag" 
+    [angularticsProperties]="{'productId': '124', 'productName': 'Pears', 'quantity': '5', 'unitPrice': '1.99', 'categoryId': '778'}"
+>Click Me for cmCreateShopAction5Tag</div>
+
+<div 
+    angulartics2On="click" 
+    angularticsAction="cmCreateShopAction9Tag" 
+    [angularticsProperties]="{'productId': '125', 'productName': 'Apricots', 'quantity': '6', 'unitPrice': '2.99', 'registrationId': '123456', 'orderId': '456', 'orderSubtotal': '29.90', 'categoryId': '779'}"
+>Click Me for cmCreateShopAction9Tag</div>
+
+<div 
+    angulartics2On="click" 
+    angularticsAction="cmCreateOrderTag" 
+    [angularticsProperties]="{'orderId': '456', 'orderSubtotal': '29.90', 'orderShipping': '0', 'registrationId': '123456', 'registrantCity': 'Atlanta', 'registrantState': 'GA', 'registrantPostalCode': '30300'}"
+>Click Me for cmCreateOrderTag</div>
+
+<div 
+    angulartics2On="click" 
+    angularticsAction="cmCreateRegistrationTag" 
+    [angularticsProperties]="{'registrationId': '123456', 'registrantEmail': 'john@example.com', 'registrantCity': 'Atlanta', 'registrantState': 'GA', 'registrantPostalCode': '30300', 'registrantCountry': 'US', 'attrbute': '3-_-SMOKING-_-DOUBLE'}"
+>Click Me for cmCreateRegistrationTag</div>
+
+<div 
+    angulartics2On="click" 
+    angularticsAction="cmCreateElementTag" 
+    [angularticsProperties]="{'elementId': 'element123456', 'elementCategory': 'some elements'}"
+>Click Me for cmCreateElementTag</div>
+
+<div 
+    angulartics2On="click" 
+    angularticsAction="cmCreateConversionEventTag" 
+    [angularticsProperties]="{'eventId': 'Request More Information', 'actionType': '2', 'eventCategoryId': 'Forms', 'points': '10'}"
+>Click Me for cmCreateConversionEventTag</div>
+```
---
+++ src/lib/providers/ibm-digital-analytics/ibm-digital-analytics.spec.ts
@@ -0,0 +1,250 @@
+import {
+    fakeAsync,
+    inject,
+    ComponentFixture,
+    TestBed,
+} from '@angular/core/testing';
+
+import { Angulartics2 } from 'angulartics2';
+import { advance, createRoot, RootCmp, TestModule } from '../../test.mocks';
+import { Angulartics2IBMDigitalAnalytics } from './ibm-digital-analytics';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+declare var window: any;
+
+describe('Angulartics2IBMDigitalAnalytics', () => {
+    let fixture: ComponentFixture<any>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [TestModule],
+            providers: [Angulartics2IBMDigitalAnalytics],
+        });
+        window.console = jasmine.createSpyObj('console', ['warn', 'log']);
+
+    });
+
+    describe('on init', () => {
+        beforeEach(() => {
+            window.console = jasmine.createSpyObj('console', ['warn', 'log']);
+        });
+
+        it('should complain if global IBM Digital Analytics functions were not loaded',
+            fakeAsync(inject([Angulartics2, Angulartics2IBMDigitalAnalytics],
+                (angulartics2: Angulartics2, angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) => {
+                    window.cmCreatePageviewTag = undefined;
+                    fixture = createRoot(RootCmp);
+                    advance(fixture);
+                    expect(console.warn).toHaveBeenCalled();
+                }),
+            ),
+        );
+    });
+
+    describe('while active', () => {
+        beforeEach(() => {
+            window.cmCreatePageviewTag = jasmine.createSpy('cmCreatePageviewTag');
+            window.cmCreateProductviewTag = jasmine.createSpy('cmCreateProductviewTag');
+            window.cmCreateShopAction5Tag = jasmine.createSpy('cmCreateShopAction5Tag');
+            window.cmCreateShopAction9Tag = jasmine.createSpy('cmCreateShopAction9Tag');
+            window.cmCreateOrderTag = jasmine.createSpy('cmCreateOrderTag');
+            window.cmCreateRegistrationTag = jasmine.createSpy('cmCreateRegistrationTag');
+            window.cmCreateElementTag = jasmine.createSpy('cmCreateElementTag');
+            window.cmCreateConversionEventTag = jasmine.createSpy('cmCreateConversionEventTag');
+            window.cmDisplayShops = jasmine.createSpy('cmDisplayShops');
+        });
+
+        it('should track pages',
+            fakeAsync(inject([Angulartics2, Angulartics2IBMDigitalAnalytics],
+                (angulartics2: Angulartics2, angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) => {
+                    fixture = createRoot(RootCmp);
+                    angulartics2.pageTrack.next({ path: '/abc' });
+                    advance(fixture);
+                    expect(window.cmCreatePageviewTag).toHaveBeenCalledWith('/abc', null, null, null);
+                }),
+            ),
+        );
+
+        it('should track cmCreateProductviewTag event',
+            fakeAsync(inject([Angulartics2, Angulartics2IBMDigitalAnalytics],
+                (angulartics2: Angulartics2, angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) => {
+                    fixture = createRoot(RootCmp);
+                    angulartics2.eventTrack.next({
+                        action: 'cmCreateProductviewTag',
+                        properties: {
+                            productId: '123',
+                            productName: 'Apples',
+                            categoryId: 'Fruits',
+                            attrbute: 'attributes',
+                            virtualCategory: 'virtual category'
+                        },
+                    });
+                    advance(fixture);
+                    expect(window.cmCreateProductviewTag).toHaveBeenCalledWith('123', 'Apples', 'Fruits', 'attributes', 'virtual category');
+                }),
+            ),
+        );
+
+
+        it('should track cmCreateShopAction5Tag event',
+            fakeAsync(inject([Angulartics2, Angulartics2IBMDigitalAnalytics],
+                (angulartics2: Angulartics2, angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) => {
+                    fixture = createRoot(RootCmp);
+                    angulartics2.eventTrack.next({
+                        action: 'cmCreateShopAction5Tag',
+                        properties: {
+                            productId: '123',
+                            productName: 'Apples',
+                            quantity: '5',
+                            unitPrice: '1.99',
+                            categoryId: 'Fruits',
+                            attrbute: 'attributes',
+                            extraFields: 'extra',
+                            virtualCategory: 'virtual category'
+                        },
+                    });
+
+                    advance(fixture);
+                    expect(window.cmCreateShopAction5Tag).toHaveBeenCalledWith('123', 'Apples', '5', '1.99', 'Fruits', 'attributes',
+                                                                                'extra', 'virtual category');
+                }),
+            ),
+        );
+
+        it('should track cmCreateShopAction9Tag event',
+            fakeAsync(inject([Angulartics2, Angulartics2IBMDigitalAnalytics],
+                (angulartics2: Angulartics2, angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) => {
+                    fixture = createRoot(RootCmp);
+                    angulartics2.eventTrack.next({
+                        action: 'cmCreateShopAction9Tag',
+                        properties: {
+                            productId: '123',
+                            productName: 'Apples',
+                            quantity: '5',
+                            unitPrice: '1.99',
+                            registrationId: '001',
+                            orderId: '777',
+                            orderSubtotal: '19.90',
+                            categoryId: 'Fruits',
+                            attrbute: 'attributes',
+                            extraFields: 'extra'
+                        },
+                    });
+
+                    advance(fixture);
+                    expect(window.cmCreateShopAction9Tag).toHaveBeenCalledWith('123', 'Apples', '5', '1.99', '001', '777', '19.90',
+                                                                                'Fruits', 'attributes', 'extra');
+                }),
+            ),
+        );
+
+        it('should track cmCreateOrderTag event',
+            fakeAsync(inject([Angulartics2, Angulartics2IBMDigitalAnalytics],
+                (angulartics2: Angulartics2, angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) => {
+                    fixture = createRoot(RootCmp);
+                    angulartics2.eventTrack.next({
+                        action: 'cmCreateOrderTag',
+                        properties: {
+                            orderId: '777',
+                            orderSubtotal: '19.90',
+                            orderShipping: '1.00',
+                            registrationId: '001',
+                            registrantCity: 'Atlanta',
+                            registrantState: 'GA',
+                            registrantPostalCode: '30303',
+                            attrbute: 'attributes',
+                            extraFields: 'extra',
+                        },
+                    });
+
+                    advance(fixture);
+                    expect(window.cmCreateOrderTag).toHaveBeenCalledWith('777', '19.90', '1.00', '001', 'Atlanta', 'GA', '30303',
+                                                                         'attributes', 'extra');
+                }),
+            ),
+        );
+
+        it('should track cmCreateRegistrationTag event',
+            fakeAsync(inject([Angulartics2, Angulartics2IBMDigitalAnalytics],
+                (angulartics2: Angulartics2, angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) => {
+                    fixture = createRoot(RootCmp);
+                    angulartics2.eventTrack.next({
+                        action: 'cmCreateRegistrationTag',
+                        properties: {
+                            registrationId: '001',
+                            registrantEmail: 'john@doe.com',
+                            registrantCity: 'Atlanta',
+                            registrantState: 'GA',
+                            registrantPostalCode: '30303',
+                            registrantCountry: 'US',
+                            attrbute: 'attributes'
+                        },
+                    });
+
+                    advance(fixture);
+                    expect(window.cmCreateRegistrationTag).toHaveBeenCalledWith('001', 'john@doe.com', 'Atlanta', 'GA', '30303', 'US',
+                                                                                 'attributes');
+                }),
+            ),
+        );
+
+        it('should track cmCreateElementTag event',
+            fakeAsync(inject([Angulartics2, Angulartics2IBMDigitalAnalytics],
+                (angulartics2: Angulartics2, angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) => {
+                    fixture = createRoot(RootCmp);
+                    angulartics2.eventTrack.next({
+                        action: 'cmCreateElementTag',
+                        properties: {
+                            elementId: 'form_001',
+                            elementCategory: 'forms',
+                            attrbute: 'attributes'
+                        },
+                    });
+
+                    advance(fixture);
+                    expect(window.cmCreateElementTag).toHaveBeenCalledWith('form_001', 'forms', 'attributes');
+                }),
+            ),
+        );
+
+        it('should track cmCreateConversionEventTag event',
+            fakeAsync(inject([Angulartics2, Angulartics2IBMDigitalAnalytics],
+                (angulartics2: Angulartics2, angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) => {
+                    fixture = createRoot(RootCmp);
+                    angulartics2.eventTrack.next({
+                        action: 'cmCreateConversionEventTag',
+                        properties: {
+                            eventId: 'event_001',
+                            actionType: 'form submitted',
+                            eventCategoryId: 'forms',
+                            points: '10.00',
+                            attrbute: 'attributes',
+                            extraFields: 'extra'
+                        },
+                    });
+
+                    advance(fixture);
+                    expect(window.cmCreateConversionEventTag).toHaveBeenCalledWith('event_001', 'form submitted', 'forms', '10.00',
+                                                                                     'attributes', 'extra');
+                }),
+            ),
+        );
+
+
+        it('should complain for unsupported event type',
+            fakeAsync(inject([Angulartics2, Angulartics2IBMDigitalAnalytics],
+                (angulartics2: Angulartics2, angulartics2IBMDigitalAnalytics: Angulartics2IBMDigitalAnalytics) => {
+                    fixture = createRoot(RootCmp);
+                    angulartics2.eventTrack.next({
+                        action: 'unsupported event',
+                        properties: {},
+                    });
+
+                    advance(fixture);
+                    expect(console.warn).toHaveBeenCalled();
+                }),
+            ),
+        );
+    });
+
+});
