145dbe2bf5e2caeb0499634bf415fc1cc2c69347
Scott Cooper <scttcper@gmail.com>
2018-10-09 14:25:47 -0700

    docs: direct users to the instructions on the provider readme

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { MatIconRegistry } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styles: [`
-    .mat-icon {
+    svg {
       width: 1.1rem !important;
       max-height: 1.1rem !important;
       vertical-align: text-top !important;
@@ -15,12 +13,4 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class NavbarComponent {
   navbarCollapsed = true;
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-      'github',
-      sanitizer.bypassSecurityTrustResourceUrl(
-        `assets/svg/github.svg`,
-      ),
-    );
-  }
 }
