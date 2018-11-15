da924c5ed741ea9e87805a1f2797c1942dcbd72c
Jan Exner <jexner@adobe.com>
2018-11-15 19:16:58 +0100

    Added provider for Launch, by Adobe

---
+++ src/lib/providers/launch/launch.spec.ts
@@ -0,0 +1,76 @@
+import { fakeAsync, inject, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Angulartics2 } from 'angulartics2';
+import { advance, createRoot, RootCmp, TestModule } from '../../test.mocks';
+import { Angulartics2LaunchByAdobe } from './launch';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+declare var window: any;
+
+describe('Angulartics2LaunchByAdobe', () => {
+  let _satellite: any;
+  let fixture: ComponentFixture<any>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TestModule],
+      providers: [Angulartics2LaunchByAdobe],
+    });
+
+    window._satellite = _satellite = {};
+    _satellite.track = function(eventID: String, payload: any) {
+      _satellite.output = {
+        "eventID" : eventID,
+        "payload": payload
+      }
+    };
+    _satellite.output = null;
+    const provider: Angulartics2LaunchByAdobe = TestBed.get(Angulartics2LaunchByAdobe);
+    provider.startTracking();
+  });
+
+  it('should track pages',
+    fakeAsync(inject([Angulartics2, Angulartics2LaunchByAdobe],
+      (angulartics2: Angulartics2, Angulartics2LaunchByAdobe: Angulartics2LaunchByAdobe) => {
+        fixture = createRoot(RootCmp);
+        angulartics2.pageTrack.next({ path: '/abc' });
+        advance(fixture);
+        expect(_satellite.output).toContain({
+          "eventID": 'pageTrack',
+          "payload": '/abc'
+        });
+      },
+    )),
+  );
+
+  it('should track events',
+    fakeAsync(inject([Angulartics2, Angulartics2LaunchByAdobe],
+      (angulartics2: Angulartics2, Angulartics2LaunchByAdobe: Angulartics2LaunchByAdobe) => {
+        fixture = createRoot(RootCmp);
+        angulartics2.eventTrack.next({ action: 'do', properties: { category: 'cat' } });
+        advance(fixture);
+        expect(_satellite.output).toContain({
+          "eventID": 'eventTrack',
+          "payload": {
+            "action": 'do',
+            "eventProperties": {
+              "category": 'cat'
+            }
+          }
+        });
+      }
+    )),
+  );
+
+  it('should set username',
+    fakeAsync(inject([Angulartics2, Angulartics2LaunchByAdobe],
+      (angulartics2: Angulartics2, Angulartics2LaunchByAdobe: Angulartics2LaunchByAdobe) => {
+        fixture = createRoot(RootCmp);
+        angulartics2.setUsername.next('testuser');
+        advance(fixture);
+        expect(angulartics2.settings.gtm.userId).toBe('testuser');
+      }
+    )),
+  );
+
+});
---
+++ src/lib/providers/launch/launch.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+
+import { Angulartics2 } from 'angulartics2';
+
+export interface Angulartics2LaunchByAdobeDCRIDMap {
+  [details: string] : string;
+}
+
+declare const _satellite: any;
+declare var payload: any;
+declare var dcrIDs: Angulartics2LaunchByAdobeDCRIDMap;
+
+@Injectable({ providedIn: 'root' })
+export class Angulartics2LaunchByAdobe {
+
+  constructor(
+    protected angulartics2: Angulartics2,
+  ) {
+    // define DCR IDs for the three call types
+    dcrIDs = {};
+    dcrIDs['pageTrack'] = 'pageTrack';
+    dcrIDs['eventTrack'] = 'eventTrack';
+    if (typeof _satellite === 'undefined') {
+      console.warn('Launch not found!');
+    }
+    this.angulartics2.setUsername
+      .subscribe((x: string) => this.setUsername(x));
+    this.angulartics2.setUserProperties
+      .subscribe((x) => this.setUserProperties(x));
+  }
+
+  setUsername(userId: string | boolean) {
+    if ('undefined' !== typeof userId && userId) {
+      payload = payload || {};
+      payload.userId = userId;
+    }
+  }
+
+  setUserProperties(properties: any) {
+    if ('undefined' !== typeof properties && properties) {
+      payload = payload || {};
+      payload.properties = properties;
+    }
+  }
+
+  startTracking() {
+    this.angulartics2.pageTrack
+      .pipe(this.angulartics2.filterDeveloperMode())
+      .subscribe((x) => this.pageTrack(x.path));
+    this.angulartics2.eventTrack
+      .pipe(this.angulartics2.filterDeveloperMode())
+      .subscribe((x) => this.eventTrack(x.action, x.properties));
+  }
+
+  pageTrack(path: string) {
+    if ('undefined' !== typeof _satellite && _satellite) {
+      _satellite.track(dcrIDs['pageTrack'], path);
+    }
+  }
+
+  /**
+   * @param action associated with the event
+   * @param properties associated with the event
+   */
+  eventTrack(action: string, properties: any) {
+    properties = properties || {};
+
+    // add properties to payload
+    payload = payload || {};
+    payload.action = action;
+    payload.eventProperties = properties;
+
+    if ('undefined' !== typeof _satellite && _satellite) {
+      _satellite.track(dcrIDs['eventTrack'], payload);
+    }
+  }
+}
