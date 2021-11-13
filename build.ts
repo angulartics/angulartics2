df36a69a79c5f5a66bfbb87e636f6860ae516601
Michał Mrozek <michalmrozek@wp.pl>
2021-11-13 14:29:54 +0100

    feat!: support angular 13

--- build.ts
+++ build.ts
@@ -6,15 +6,16 @@ import { ngPackagr } from 'ng-packagr';
 async function main() {
   await ngPackagr()
     .forProject(join(process.cwd(), 'src/lib/package.json'))
+    .withTsConfig(join(process.cwd(), 'tsconfig.json'))
     .build();
 
-    copyFileSync('README.md', join(process.cwd(), 'dist/README.md'));
-    copyFileSync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
+  copyFileSync('README.md', join(process.cwd(), 'dist/README.md'));
+  copyFileSync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
 }
 
 main()
   .then(() => console.log('success'))
-  .catch((e) => {
+  .catch(e => {
     console.error(e);
     process.exit(1);
   });
