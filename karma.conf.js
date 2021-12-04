a8559b79463e5806cfd6e765f91c6221507c61a7
Michał Mrozek <Michsior14@users.noreply.github.com>
2021-12-04 06:59:37 +0100

    feat: add angular 13 support (#434)
    BREAKING CHANGE: support for angular 13
    
    fixes: #433

--- karma.conf.js
+++ karma.conf.js
@@ -9,14 +9,15 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, './coverage/zzz'),
+      subdir: '.',
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
